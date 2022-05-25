d9813d0bb31e8cd0d9beca9386be8d3ebc048aa4
jlxip <jlxip@protonmail.com>
2022-05-25 23:11:14 +0200

    Test RPC

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <syscalls>
 #include <common.hpp>
 #include <cstring>
+#include <export>
+
+size_t asdf(size_t client, size_t x) {
+	IGNORE(client);
+	writec(x);
+	return 42;
+}
+
+//extern "C" void rpcEntry();
 
 extern "C" void _start(size_t fb, size_t row, size_t col) {
 	init(fb, row, col);
 	writes("[OK]", 4);
 
+	std::exportProcedure((void*)asdf, 1);
+	std::enableRPC(/*rpcEntry*/);
 	std::publish("term");
 	std::halt();
 }
